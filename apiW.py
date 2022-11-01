0b2111e5e6d31c1a278569b8f12f71bb68e2104f
MuhammedEminK <83522521+MuhammedEminK@users.noreply.github.com>
2022-11-01 23:19:44 +0300

    Update apiW.py

--- apiW.py
+++ apiW.py
@@ -27,5 +27,5 @@ async def now(item: Coordinate,):
 
     return x
 
-if __name__=="__main__":
-    uvicorn.run("apiW:app",host='0.0.0.0', port=8000, reload=True)
+#if __name__=="__main__":
+#    uvicorn.run("apiW:app",host='0.0.0.0', port=8000, reload=True)
